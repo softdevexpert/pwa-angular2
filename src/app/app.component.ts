b8ac8820b8ec6fbb7fafc89f8e908a23eaa3439c
xiong jin <xiongjin402@gmail.com>
2019-02-21 05:31:13 +0800

    first commit (pwademo angular project)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from './api.service';
+import { Item } from './api.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector:  'app-root',
+  templateUrl:  './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pwademo';
+export  class  AppComponent  implements  OnInit {
+  title  =  'pwademo';
+  items:  Array<Item>;
+  constructor(private  apiService:  ApiService) {
+  }
+  ngOnInit() {
+    this.fetchData();
+  }
+  fetchData() {
+    this.apiService.fetch().subscribe((data:  Array<Item>) => {
+      console.log(data);
+      this.items  =  data;
+    }, (err) => {
+      console.log(err);
+    });
+  }
 }
